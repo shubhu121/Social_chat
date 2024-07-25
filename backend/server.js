7708074ee869b7c058f58989d57c2f9ec40c2f40
shubhu121 <shubhucsrldelhi@gmail.com>
2024-07-25 19:37:10 +0530

    backend update

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-const express = require("express")
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    // root route http://localhost:5000/
+    res.send("server is ready");
+})
+
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
